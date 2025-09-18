d9ef60071c3f5adb2671793474ed9cb5f04b05ed
rabochee-zerkalo-kraken <linhtogia54@gmail.com>
2025-09-19 05:05:54 +0700

    writerrottt.py

---
+++ writerrottt.py
@@ -0,0 +1,11 @@
+def ascii_photo(text="PYTHON"):
+    art = f"""
+   .~~~~.
+  :{text.center(6)}:
+   '~~~~'
+    |  |
+   /____\\
+    """
+    print(art)
+
+ascii_photo("CODE")  # Попробуй свои слова!
